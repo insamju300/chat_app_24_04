f32400e091e20b351035790ace222766210366d1
insamju300 <insamju300@gmail.com>
2024-04-16 14:07:32 +0900

    소스재등록

---
+++ src/main/java/com/koreait/exam/chat_app_24_04/ChatController.java
@@ -0,0 +1,86 @@
+package com.koreait.exam.chat_app_24_04;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Controller
+@Slf4j
+@RequestMapping("/chat")
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final SseEmitters sseEmitters;
+
+    private List<ChatMessage> chatMessages = new ArrayList<>();
+
+    public record writeMessageRequest(String authorName, String content) {
+    }
+
+    public record writeMessageResponse(long id) {
+
+    }
+
+    @GetMapping("/room")
+    public String showRoom() {
+        return "chat/room";
+    }
+
+    @PostMapping("/writeMessage")
+    @ResponseBody
+    public RsData<writeMessageResponse> writeMessage(@RequestBody writeMessageRequest req) {
+        ChatMessage message = new ChatMessage(req.authorName, req.content);
+        chatMessages.add(message);
+
+        sseEmitters.noti("chat__messageAdded");
+
+        return new RsData<>(
+                "S-1",
+                "메세지가 작성됨",
+                new writeMessageResponse(message.getId())
+        );
+    }
+
+    public record MessagesRequest(Long fromId) {
+
+    }
+
+    public record MessagesResponse(List<ChatMessage> messages, long count) {
+
+    }
+
+    @GetMapping("/messages")
+    @ResponseBody
+    public RsData<MessagesResponse> messages(MessagesRequest req) {
+//        log.debug("req : {}",req);
+
+        List<ChatMessage> messages = chatMessages;
+
+        // 번호가 입력 되었다면
+        if (req.fromId != null) {
+            // 해당 번호의 채팅 메세지가 전체 리스트의 몇번째 인덱스인지? 없다면
+            int index = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == req.fromId)
+                    .findFirst()
+                    .orElse(-1);
+
+            if (index != -1) {
+                // 만약에 index가 -1이 아니라면? 0번 부터 index번 까지 제거한 리스트를 만든다.
+                messages = messages.subList(index + 1, messages.size());
+            }
+        }
+
+
+        return new RsData<>(
+                "S-1",
+                "성공",
+                new MessagesResponse(messages, messages.size())
+        );
+
+    }
+}
